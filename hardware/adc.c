b6cd0885168183adca4fc843ca82a23cec21d6ba
Song Muchun <smuchun@gmail.com>
2019-03-17 22:23:54 +0800

    remove ADC #define from adc.h to adc.c

--- hardware/adc.c
+++ hardware/adc.c
@@ -1,6 +1,14 @@
 #include "adc.h"
 #include "delay.h"
 
+#define ADC_POWER               0x80
+#define ADC_FLAG                0x10
+#define ADC_START               0x08
+#define ADC_SPEEDLL             0x00
+#define ADC_SPEEDL              0x20
+#define ADC_SPEEDH              0x40
+#define ADC_SPEEDHH             0x60
+
 void adc_init(unsigned char channel)
 {
 	channel &= BIT(0) | BIT(1) | BIT(3);

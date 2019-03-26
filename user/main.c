ccbccc64c4d5973381c663014d536de550bfc49b
Song Muchun <smuchun@gmail.com>
2019-03-27 01:01:37 +0800

    Fix forgot to call buzzer_power_on()

--- user/main.c
+++ user/main.c
@@ -497,7 +497,7 @@ void main(void)
 	struct fb_info idata *fb_info = &user_data.fb_info;
 
 	user_data_init();
-	// buzzer_power_on();
+	buzzer_power_on();
 	fb_off();
 	fb_matrixs_test();
 	uart_init();

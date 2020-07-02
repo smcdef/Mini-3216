51aae1338b1e25f615db597a78efe5fa725b6a05
Muchun Song <songmuchun@bytedance.com>
2020-07-02 22:02:03 +0800

    Remove unused include
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- core/fb_core.c
+++ core/fb_core.c
@@ -10,9 +10,7 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  */
-
 #include "fb_core.h"
-#include <string.h>
 
 /**
  * +---------------+---------------+---------------+---------------+
